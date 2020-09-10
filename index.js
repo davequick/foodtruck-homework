4e1e95e34add0f33a211e1ce2219ef7e88aa448e
Dave Quick <dq@dq.gg>
2020-09-10 18:19:02 -0400

    quick refactor to chuck out to some functions rather than all inline.

--- index.js
+++ index.js
@@ -6,65 +6,81 @@ const _ = require('lodash');
 /*
  * Read Data Async
  */
-const dataPromise = new Promise(function(resolve, reject) {
-    fs.readFile('./Mobile_Food_Facility_Permit.csv', 'utf8' , (err, data) => {
-        if (err) {
-            reject(err);
-            return
-        }
-        resolve(data);
-    })
-});
+function readFoodTruckCSV(filename) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+                return
+            }
+            resolve(data);
+        })
+    });
+}
 
 /*
  * parse the csv
  */
-const parsedDataPromise = new Promise(function(resolve, reject){
-    dataPromise.then((data) => {
-        const parsedData = [];
-        const stream = parse({headers: true})
-            .on('error', error => reject(error))
-            .on('data', row => parsedData.push(row))
-            .on('end', () => resolve(parsedData));
-        stream.write(data);
-        stream.end();
+function parseCsvFile(csv) {
+    return new Promise(function (resolve, reject) {
+        csv.then((data) => {
+            const parsedData = [];
+            const stream = parse({headers: true})
+                .on('error', error => reject(error))
+                .on('data', row => parsedData.push(row))
+                .on('end', () => resolve(parsedData));
+            stream.write(data);
+            stream.end();
+        });
     });
-});
-
-/*
- * dummy value for lat/lon until we determine how user provides location - this is generic "san francisco" form google
- */
-const locationOfInterest =     {lat: 37.7749, lon: -122.4194};
-
-/*
- * for a given item, calculate distance in miles as the crow flies
- */
-function determineDistance(foodtruck) {
-    foodtruck.distance = geodist(locationOfInterest, {lat:foodtruck.Latitude, lon: foodtruck.Longitude}, {exact: true, unit: 'miles'});
-    return foodtruck;
 }
 
 /*
  * for each item in the array passed, calculate the distance to it
  */
-function calculateFoodTruckDistance(parsedData) {
-    return _.map(parsedData, determineDistance);
+function calculateDistance(locationOfInterest, data) {
+    return _.map(data, (foodtruck)=> {
+        foodtruck.distance =
+            geodist(locationOfInterest,
+                {lat:foodtruck.Latitude, lon: foodtruck.Longitude},
+                {exact: true, unit: 'miles'});
+        return foodtruck;
+    });
 }
 
 /*
  * sort an array of foodtrucks based on the calculated distances
  */
-function sortFoodTrucksByDistance(locationEnhancedData) {
-    return _.orderBy(locationEnhancedData, "distance");
+function sortFoodTrucksByDistance(locationEnhancedFoodtrucks) {
+    return _.orderBy(locationEnhancedFoodtrucks, "distance");
 }
+
 
 /*
  * when promise is fulfilled by the csv parser and we have an appropriate array of all food trucks in it,
  * process that list
  */
-parsedDataPromise.then((parsedData) => {
-    const locationEnhancedData = calculateFoodTruckDistance(parsedData);
-    const distanceSortedFoodtrucks = sortFoodTrucksByDistance(locationEnhancedData);
-    const closestFive = _.slice(distanceSortedFoodtrucks, 0, 4);
-    console.log(closestFive);
-});
+function returnClosestNFoodtrucks(locationOfInterest, data,n) {
+    return new Promise(function (resolve) {
+        data.then((data) => {
+            const locationEnhancedFoodtrucks = calculateDistance(locationOfInterest, data);
+            const distanceSortedFoodtrucks = sortFoodTrucksByDistance(locationEnhancedFoodtrucks);
+            resolve(_.slice(distanceSortedFoodtrucks, 0, n)); // closest n sorted by distance
+        });
+    });
+}
+
+
+// main
+if (require.main === module) {
+    /*
+     * dummy value for lat/lon until we determine how user provides location - this is generic "san francisco" form google
+     */
+    const locationOfInterest = {lat: 37.7749, lon: -122.4194};
+    const csv = readFoodTruckCSV("./Mobile_Food_Facility_Permit.csv");
+    const data = parseCsvFile(csv);
+    const result = returnClosestNFoodtrucks(locationOfInterest, data, 5)
+    result.then((result) => console.log(result));
+}
+
+
