76d7f8e4619b442b8f834d0bdd8b0ee1f957218f
Dave Quick <dq@dq.gg>
2020-09-10 17:39:26 -0400

    check in the csv we were given as an example, and a working MVP against a fixed opint specified in code.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const { parse } = require('fast-csv');
+const geodist = require('geodist');
+const _ = require('lodash');
 
 /*
  * Read Data Async
@@ -23,10 +25,46 @@ const parsedDataPromise = new Promise(function(resolve, reject){
         const stream = parse({headers: true})
             .on('error', error => reject(error))
             .on('data', row => parsedData.push(row))
-            .on('end', rowCount => resolve({parsedData, rowCount}));
+            .on('end', () => resolve(parsedData));
         stream.write(data);
         stream.end();
     });
 });
 
-parsedDataPromise.then((parsedData)=> console.log(parsedData));
+/*
+ * dummy value for lat/lon until we determine how user provides location - this is generic "san francisco" form google
+ */
+const locationOfInterest =     {lat: 37.7749, lon: -122.4194};
+
+/*
+ * for a given item, calculate distance in miles as the crow flies
+ */
+function determineDistance(foodtruck) {
+    foodtruck.distance = geodist(locationOfInterest, {lat:foodtruck.Latitude, lon: foodtruck.Longitude}, {exact: true, unit: 'miles'});
+    return foodtruck;
+}
+
+/*
+ * for each item in the array passed, calculate the distance to it
+ */
+function calculateFoodTruckDistance(parsedData) {
+    return _.map(parsedData, determineDistance);
+}
+
+/*
+ * sort an array of foodtrucks based on the calculated distances
+ */
+function sortFoodTrucksByDistance(locationEnhancedData) {
+    return _.orderBy(locationEnhancedData, "distance");
+}
+
+/*
+ * when promise is fulfilled by the csv parser and we have an appropriate array of all food trucks in it,
+ * process that list
+ */
+parsedDataPromise.then((parsedData) => {
+    const locationEnhancedData = calculateFoodTruckDistance(parsedData);
+    const distanceSortedFoodtrucks = sortFoodTrucksByDistance(locationEnhancedData);
+    const closestFive = _.slice(distanceSortedFoodtrucks, 0, 4);
+    console.log(closestFive);
+});
